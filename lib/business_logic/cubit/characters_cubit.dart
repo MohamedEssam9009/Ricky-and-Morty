154487534251a2256767995b88c9bd7713f2787f
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2024-02-01 23:55:32 +0200

    finish the cubit & state class

--- lib/business_logic/cubit/characters_cubit.dart
+++ lib/business_logic/cubit/characters_cubit.dart
@@ -1,6 +1,18 @@
 import 'package:bloc_example/business_logic/cubit/characters_state.dart';
+import 'package:bloc_example/data/models/characters.dart';
+import 'package:bloc_example/data/repository/characters_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CharactersCubit extends Cubit<CharactersState> {
-  CharactersCubit() : super(CharactersInitial());
+  final CharactersRepository charactersRepository;
+  late List<Character> characters;
+
+  CharactersCubit(this.charactersRepository) : super(CharactersInitial());
+  List<Character> getAllCharacters() {
+   charactersRepository.getAllCharacters().then((characters) {
+      emit(CharactersLoaded(characters));
+      this.characters = characters;
+    });
+    return characters;
+  }
 }
--- lib/business_logic/cubit/characters_state.dart
+++ lib/business_logic/cubit/characters_state.dart
@@ -1,4 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:bloc_example/data/models/characters.dart';
 
 abstract class CharactersState {}
 
 class CharactersInitial extends CharactersState {}
+
+class CharactersLoaded extends CharactersState {
+  List<Character> characters;
+  CharactersLoaded(this.characters);
+}
