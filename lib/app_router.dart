7e094e02f07b29dc33a755068d948b0745464c3f
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2024-02-02 00:57:18 +0200

    added a BlocProvider

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,18 +1,40 @@
+import 'package:bloc_example/business_logic/cubit/characters_cubit.dart';
 import 'package:bloc_example/constants/strings.dart';
+import 'package:bloc_example/data/repository/characters_repository.dart';
 import 'package:bloc_example/presentation/screens/characters_details_screen.dart';
 import 'package:bloc_example/presentation/screens/characters_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'data/web_services/characters_web_services.dart';
 
 class AppRouter {
+  late CharactersRepository charactersRepository;
+  late CharactersCubit charactersCubit;
+
+  AppRouter() {
+    charactersRepository = CharactersRepository(CharactersWebServices());
+    charactersCubit = CharactersCubit(charactersRepository);
+  }
+
   Route? onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case charactersScreen:
-        return MaterialPageRoute(builder: (_) => const CharactersScreen());
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider<CharactersCubit>(
+            create: (context) => charactersCubit,
+            child: const CharactersScreen(),
+          ),
+        );
       case charactersDetailsScreen:
-        return MaterialPageRoute(builder: (_) => const CharactersDetailsScreen());
+        return MaterialPageRoute(
+          builder: (_) => const CharactersDetailsScreen(),
+        );
 
       default:
-        return MaterialPageRoute(builder: (_) => const Scaffold());
+        return MaterialPageRoute(
+          builder: (_) => const Scaffold(),
+        );
     }
   }
 }
