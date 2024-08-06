0ea9ad2d9f63d87be66c7b5db0dcea5e5d1dce77
Mohamed Essam <77198018+MohamedEssam9009@users.noreply.github.com>
2024-08-07 00:06:37 +0300

    fix some commas

--- lib/presentation/screens/characters_screen.dart
+++ lib/presentation/screens/characters_screen.dart
@@ -61,21 +61,23 @@ class _CharactersScreenState extends State<CharactersScreen> {
 
   Widget buildBlocWidget() {
     return BlocBuilder<CharactersCubit, CharactersState>(
-        builder: (context, state) {
-      if (state is CharactersLoaded) {
-        allCharacters = (state).characters;
-        return buildLoadedListWidgets();
-      } else {
-        return showLoadingIndicator();
-      }
-    });
+      builder: (context, state) {
+        if (state is CharactersLoaded) {
+          allCharacters = (state).characters;
+          return buildLoadedListWidgets();
+        } else {
+          return showLoadingIndicator();
+        }
+      },
+    );
   }
 
   Widget showLoadingIndicator() {
     return const Center(
-        child: CircularProgressIndicator(
-      color: MyColors.myYellow,
-    ));
+      child: CircularProgressIndicator(
+        color: MyColors.myYellow,
+      ),
+    );
   }
 
   Widget buildLoadedListWidgets() {
@@ -93,32 +95,33 @@ class _CharactersScreenState extends State<CharactersScreen> {
 
   Widget buildCharactersList() {
     return GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 2 / 3,
-          crossAxisSpacing: 1,
-          mainAxisSpacing: 1,
-        ),
-        shrinkWrap: true,
-        physics: const ClampingScrollPhysics(),
-        padding: EdgeInsets.zero,
-        itemCount: _searchTextController.text.isEmpty
-            ? allCharacters.length
-            : searchedForCharacters.length,
-        itemBuilder: (context, index) {
-          return CharacterItem(
-            character: _searchTextController.text.isEmpty
-                ? allCharacters[index]
-                : searchedForCharacters[index],
-          );
-        });
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 2 / 3,
+        crossAxisSpacing: 1,
+        mainAxisSpacing: 1,
+      ),
+      shrinkWrap: true,
+      physics: const ClampingScrollPhysics(),
+      padding: EdgeInsets.zero,
+      itemCount: _searchTextController.text.isEmpty
+          ? allCharacters.length
+          : searchedForCharacters.length,
+      itemBuilder: (context, index) {
+        return CharacterItem(
+          character: _searchTextController.text.isEmpty
+              ? allCharacters[index]
+              : searchedForCharacters[index],
+        );
+      },
+    );
   }
 
   Widget _buildSearchFiled() {
     return TextField(
       controller: _searchTextController,
       cursorColor: MyColors.myGrey,
-      decoration:  InputDecoration(
+      decoration: InputDecoration(
         hintText: 'Find a character ....',
         border: InputBorder.none,
         hintStyle: TextStyle(
@@ -204,22 +207,23 @@ class _CharactersScreenState extends State<CharactersScreen> {
 
   Widget buildNoInternetWidget() {
     return Center(
-        child: Container(
-      color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SizedBox(height: 20),
-          const Text(
-            'Can\'t connect .. check internet',
-            style: TextStyle(
-              fontSize: 22,
-              color: MyColors.myGrey,
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20),
+            const Text(
+              'Can\'t connect .. check internet',
+              style: TextStyle(
+                fontSize: 22,
+                color: MyColors.myGrey,
+              ),
             ),
-          ),
-          Image.asset('assets/images/no_internet.png'),
-        ],
+            Image.asset('assets/images/no_internet.png'),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
